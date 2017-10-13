02d5ea9a53e97dc89ddc60f96e0cd0eff2f63ca5
User <User@User-PC>
2017-10-13 17:56:36 +0200

    ricorsione

---
+++ src/managerindustry/eventi/PersonaFinta.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managerindustry.eventi;
+
+/**
+ *
+ * @author Luca Coraci <luca.coraci@istc.cnr.it>
+ */
+public class PersonaFinta {
+    
+    private String nome;
+    private String cognome;
+
+    public PersonaFinta() {
+    }
+
+    public PersonaFinta(String nome, String cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    
+    
+    
+}
